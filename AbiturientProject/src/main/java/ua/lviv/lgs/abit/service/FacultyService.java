e2936f277c41bc12855be980927e242fa1651e6f
Міха <eglet777@ukr.net>
2021-09-24 12:31:06 +0300

    adding logger to my project

--- AbiturientProject/src/main/java/ua/lviv/lgs/abit/service/FacultyService.java
+++ AbiturientProject/src/main/java/ua/lviv/lgs/abit/service/FacultyService.java
@@ -2,6 +2,8 @@ package ua.lviv.lgs.abit.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,17 +12,21 @@ import ua.lviv.lgs.abit.domain.Faculty;
 
 @Service
 public class FacultyService {
+	private Logger logger = LoggerFactory.getLogger(FacultyService.class);
 	@Autowired
 	private FacultyRepository facultyRepository;
 	
 	public Faculty save(Faculty faculty) {
+		logger.info("Create new faculty : " + faculty);
 		return facultyRepository.save(faculty);
 	}
 	
 	public List<Faculty> getAll(){
+		logger.info("Get all faculties");
 		return facultyRepository.findAll();
 	}
 	public Faculty findById(Integer id) {
+		logger.info("Find faculty by id : " + id);
 		return facultyRepository.findById(id).get();
 	}
 }
